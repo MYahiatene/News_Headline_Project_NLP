4dbe4f39f29789aa93e43633032c1073c099ffe8
myahiate <mokrane_yahiatene@web.de>
2021-08-20 15:37:22 +0200

    model impl and refactored preprocessing

--- code/model.py
+++ code/model.py
@@ -1,19 +1,22 @@
 import tensorflow as tf
-from transformers import TFAutoModel
-from preprocessing import prepare_data, max_length
-
-train_data, dev_data = prepare_data()
-train_data=train_data.shuffle(100).batch(128, drop_remainder=True)
-dev_data= dev_data.shuffle(100).batch(128, drop_remainder=True)
-model = TFAutoModel.from_pretrained("bert-base-cased")
+from transformers import TFAutoModel as auto
+data_set=data_sets(tokenizer,max_length)
+train_data=data_set.data_set_train
+dev_data=data_set.data_set_dev
+print(train_data)
+print(dev_data)
+train_data=train_data.shuffle(100).batch(32, drop_remainder=True)
+dev_data= dev_data.shuffle(100).batch(32, drop_remainder=True)
+model = auto.from_pretrained("bert-base-cased")
 input = tf.keras.layers.Input(shape=(max_length,), name="input", dtype="int32")
 mask = tf.keras.layers.Input(shape=(max_length,), name="mask", dtype="int32")
 result_emb = model.bert(input, attention_mask=mask)[1]
-layer1 = tf.keras.layers.Dense(max_length * 2, activation="relu")(result_emb)
+layer1 = tf.keras.layers.Dense(max_length*2, activation="relu")(result_emb)
 layer_output = tf.keras.layers.Dense(3, activation="softmax", name="output_layer")(layer1)
 model = tf.keras.Model(inputs=[input, mask], outputs=layer_output)
-model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-5, decay=1e-6), loss=tf.keras.losses.CategoricalCrossentropy(),
+#model.layers[2].trainable=False
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5, decay=1e-6), loss=tf.keras.losses.CategoricalCrossentropy(),
               metrics=[tf.keras.metrics.CategoricalAccuracy("accuracy")])
 model.summary()
-model.layers[2].trainable=False
-history = model.fit(train_data,validation_data=dev_data, epochs=1)
+history = model.fit(train_data,validation_data=dev_data,epochs=3)
+model.save("my_model")
--- code/preprocessing.py
+++ code/preprocessing.py
@@ -8,66 +8,60 @@ bert_model = "bert-base-cased"
 tokenizer = BertTokenizer.from_pretrained(bert_model)
 max_length = 512
 
-def prepare_data():
-    train, test, dev = import_corpus()
-    labels_train = utils.to_categorical(
-        labels := train["meanGrade1"].append(train["meanGrade2"], ignore_index=True).values,
-        num_classes=len(set(labels)))
-    labels_dev = utils.to_categorical(labels := dev["meanGrade1"].append(dev["meanGrade2"], ignore_index=True).values,
-                                      num_classes=len(set(labels)))
-    data_train = data_set(train, tokenizer, max_length)
-    input_ids_train, attention_masks_train, labels_train = data_train.prepare()
-    labels_train = utils.to_categorical(labels_train, num_classes=3)
-    labels_dev = utils.to_categorical(labels_dev, num_classes=3)
- #   data_test = data_set(test, tokenizer, max_length)
-#    input_ids_test, attention_masks_test, labels_test = data_test.prepare()
-    data_dev = data_set(dev, tokenizer, max_length)
-    input_ids_dev, attention_masks_dev, labels_dev = data_dev.prepare()
-    data_set_train = tf.data.Dataset.from_tensor_slices(({"input":input_ids_train, "mask":attention_masks_train}, labels_train))
-    data_set_dev = tf.data.Dataset.from_tensor_slices(({"input":input_ids_dev, "mask":attention_masks_dev}, labels_dev))
-    return data_set_train, data_set_dev
+
+class data_sets():
+    def __init__(self, tokenizer: BertTokenizer, max_len: int = max_length):
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.train, self.test, self.dev = self._import_corpus_()
+        self.data_set_train, self.data_set_dev = self._prepare_data_()
+        self.data_set_test = self._prepare_(self.test)
 
 
-def import_corpus():
+def _import_corpus_(self):
     test = pd.read_csv("test.csv")
     train = pd.read_csv("train.csv")
     dev = pd.read_csv("dev.csv")
     return train, test, dev
 
 
-class data_set():
-    def __init__(self, data: pd.DataFrame, tokenizer: BertTokenizer, max_len: int = 128):
-        self.data = data
-        self.tokenizer = tokenizer
-        self.max_len = max_len
+def _prepare_data_(self):
+    input_ids_train, attention_masks_train, labels_train = self._prepare_(self.train)
+    input_ids_dev, attention_masks_dev, labels_dev = self._prepare_(self.dev)
 
-    def __len__(self):
-        return len(self.data)
+    data_set_train = tf.data.Dataset.from_tensor_slices(
+        ({"input": input_ids_train, "mask": attention_masks_train}, labels_train))
+    data_set_dev = tf.data.Dataset.from_tensor_slices(
+        ({"input": input_ids_dev, "mask": attention_masks_dev}, labels_dev))
+    return data_set_train, data_set_dev
 
-    def prepare(self):
-        attention_arr = np.zeros((self.__len__(), self.max_len))
-        input_ids_arr = np.zeros((self.__len__(), self.max_len))
-        labels = list()
-        for i in range(self.__len__()):
-            row = self.data.iloc[i]
-            text_original1 = row["original1"].replace("<", "").replace("/>", "")
-            text_edited1 = row["original1"].split("<")[0] + row["edit1"] + \
-                           row["original1"].split(">")[1]
-            text_original2 = row["original2"].replace("<", "").replace("/>", "")
-            text_edited2 = row["original2"].split("<")[0] + row["edit2"] + \
-                           row["original2"].split(">")[1]
-            batch = self.tokenizer.encode_plus(
-                text_original1 + " " + text_edited1 + " " + text_original2 + " " + text_edited2,
-                add_special_tokens=True,
-                max_length=self.max_len,
-                return_token_type_ids=False, padding="max_length", truncation=True,
-                return_attention_mask=True, return_tensors="tf")
-            input_ids_arr[i, :] = batch["input_ids"]
-            attention_arr[i, :] = batch["attention_mask"]
-            if row["meanGrade1"] - row["meanGrade2"] < 0:
-                labels.append(2)
-            elif row["meanGrade1"] - row["meanGrade2"] > 0:
-                labels.append(1)
-            else:
-                labels.append(0)
-        return input_ids_arr, attention_arr, labels
+
+def _prepare_(self, data):
+    attention_arr = np.zeros((len(data), self.max_len))
+    input_ids_arr = np.zeros((len(data), self.max_len))
+    labels = list()
+    for i in range(len(data)):
+        row = data.iloc[i]
+        text_original1 = row["original1"].replace("<", "").replace("/>", "")
+        text_edited1 = row["original1"].split("<")[0] + row["edit1"] + \
+                       row["original1"].split(">")[1]
+        text_original2 = row["original2"].replace("<", "").replace("/>", "")
+        text_edited2 = row["original2"].split("<")[0] + row["edit2"] + \
+                       row["original2"].split(">")[1]
+        batch = self.tokenizer.encode_plus(
+            text_original1 + " " + text_edited1 + " " + text_original2 + " " + text_edited2,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False, padding="max_length", truncation=True,
+            return_attention_mask=True, return_tensors="tf")
+        input_ids_arr[i, :] = batch["input_ids"]
+        attention_arr[i, :] = batch["attention_mask"]
+        if row["meanGrade1"] - row["meanGrade2"] < 0:
+            labels.append(2)
+        elif row["meanGrade1"] - row["meanGrade2"] > 0:
+            labels.append(1)
+        else:
+            labels.append(0)
+    labels = utils.to_categorical(labels, num_classes=len(set(labels)))
+    return input_ids_arr, attention_arr, labels
+
